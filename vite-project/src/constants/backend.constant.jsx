3fff19c44a97564a58e63d867cc66768265739c4
Laxman Anand <laxman7291@gmail.com>
2024-01-31 21:11:36 +0530

    added url

--- vite-project/src/constants/backend.constant.jsx
+++ vite-project/src/constants/backend.constant.jsx
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:3000";
-export const FRONTEND_URL = "http://127.0.0.1:5173";
+export const BACKEND_URL = "https://quiz-gxoh.onrender.com";
+export const FRONTEND_URL = "https://quiz-frontend1.onrender.com";
